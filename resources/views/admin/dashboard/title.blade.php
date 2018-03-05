adba1402425b32fdcdda799552609766538dbb3c
tyrantgenesis <genesis489449701@gmail.com>
2018-03-05 22:25:50 +0800

    Initial commit

---
+++ resources/views/admin/dashboard/title.blade.php
@@ -0,0 +1,34 @@
+<style>
+    .title {
+        font-size: 50px;
+        color: #636b6f;
+        font-family: 'Raleway', sans-serif;
+        font-weight: 100;
+        display: block;
+        text-align: center;
+        margin: 20px 0 10px 0px;
+    }
+
+    .links {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+
+    .links > a {
+        color: #636b6f;
+        padding: 0 25px;
+        font-size: 12px;
+        font-weight: 600;
+        letter-spacing: .1rem;
+        text-decoration: none;
+        text-transform: uppercase;
+    }
+</style>
+
+<div class="title">
+    Remylift Admin
+</div>
+<div class="links">
+    <a href="{{ url('/') }}" target="_blank">网站首页</a>
+    <a href="http://laravel-admin.org/docs"  target="_blank">操作文档</a>
+</div>
