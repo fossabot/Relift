53560cc615db965e033a5f630a0266d10f51ff09
tyrantgenesis <genesis489449701@gmail.com>
2018-03-26 17:27:03 +0800

    继续写API

--- app/Http/Controllers/Tool/VgController.php
+++ app/Http/Controllers/Tool/VgController.php
@@ -21,42 +21,96 @@ class VgController extends Controller
 				]
 			]
 		]);
-		$player = json_decode($response->getBody()->getContents());
-		dump($player);
+		return $player = json_decode($response->getBody()->getContents());
 	}
 
 	public function matches($name){
 		$response = \VGWrap::get('shards/cn/matches', [
 			'query' => [
-				'sort' => 'sort=-createdAt',
+				//'sort' => 'sort=-createdAt',
 				'filter' => [
-					'playerNames' => $name
+                    //'createdAt-start' => '2017-01-01T08:25:30Z',
+					'playerNames' => $name,
 				],
 				'page' => [
-					'limit' => 2
+					'limit' => 1,
+                    'offset' => 39,
 				],
 			]
 		]);
+
 		$matches = json_decode($response->getBody()->getContents());
-		dump(get_object_vars($matches));
+		dump($matches);
+
+		$player = $this->player($name);
+        $p['data'] = $player->data[0];
+		$tmp = [];
+		$m = [];
+
 		foreach ($matches->data as $key => $match){
-			$m[$key]['blue']['all'] = $match->relationships->rosters->data[0]->id;
-			$m[$key]['red']['all'] = $match->relationships->rosters->data[1]->id;
+
+			foreach ($matches->included as $k => $v) {
+
+			    foreach ($match->relationships->rosters->data as $kk => $vv){
+			        if ($v->type == 'roster'){
+			            if ($v->id == $vv->id){
+			                $tmp[$kk]['id'] = $vv->id;
+			                $m[$key][$kk]['match'] = $match->attributes;
+                        }
+                    }
+                }
+            }
+            foreach ($matches->included as $k => $v){
+			    if ($v->id == $tmp[0]['id']){
+                    $m[$key][0]['roster'] = $v->attributes;
+                    $m[$key][0]['color'] = $v->attributes->stats->side;
+                    foreach ($v->relationships->participants->data as $kk => $vv){
+                        $tmp[0]['part_id'][$kk] = $vv->id;
+                    }
+                }elseif ($v->id == $tmp[1]['id']){
+                    $m[$key][1]['roster'] = $v->attributes;
+                    $m[$key][1]['color'] = $v->attributes->stats->side;
+                    foreach ($v->relationships->participants->data as $kk => $vv){
+                        $tmp[1]['part_id'][$kk] = $vv->id;
+                    }
+                }
+
+               /* if ($v->type == 'participant'){
+                    foreach ($v->)
+                }*/
+            }
+
+            foreach ($matches->included as $k => $v){
+			    foreach ($tmp[0]['part_id'] as $kk => $vv){
+                    if ($v->id == $vv){
+                        $m[$key][0]['part_data'][$kk] = $v->attributes;
+                        $tmp[0]['player_id'][$kk] = $v->relationships->player->data->id;
+                    }
+                }
+			    foreach ($tmp[1]['part_id'] as $kk => $vv){
+                    if ($v->id == $vv){
+                        $m[$key][1]['part_data'][$kk] = $v->attributes;
+                        $tmp[1]['player_id'][$kk] = $v->relationships->player->data->id;
+                    }
+                }
+            }
+
+            foreach ($matches->included as $k => $v){
+			    foreach ($tmp[0]['player_id'] as $kk => $vv){
+                    if ($v->id == $vv){
+                        $m[$key][0]['player_data'][$kk] = $v->attributes;
+                    }
+                }
+			    foreach ($tmp[1]['player_id'] as $kk => $vv){
+                    if ($v->id == $vv){
+                        $m[$key][1]['player_data'][$kk] = $v->attributes;
+                    }
+                }
+            }
 		}
 
-		foreach ($matches->included as $key => $in){
-			foreach ($m as $k => $v){
-				if ($in->id == $v['blue']['all']){
-					$m[$k]['blue']['data'] = $in->relationships->participants->data;
-				}elseif ($in->id == $v['red']['all']){
-					$m[$k]['red']['data'] = $in->relationships->participants->data;
-				}
-
-
-			}
-		}
 		dump($m);
-		return view('theme::vg.show');
+		return view('theme::vg.show', compact('m', 'p'));
 
 	}
 
--- resources/views/themes/relift/vg/show.blade.php
+++ resources/views/themes/relift/vg/show.blade.php
@@ -1,5 +1,13 @@
 @extends('theme::home.main')
 @section('body')
+    <style>
+        .vg-red{
+            border-left: 8px solid #fe6161;
+        }
+        .vg-blue{
+            border-left: 8px solid #0aa9dc;
+        }
+    </style>
     <script>
         document.getElementsByTagName('body')[0].setAttribute('class', 'blog-posts');
     </script>
@@ -17,12 +25,41 @@
             <div class="section">
                 <div class="row">
                     <div class="col-md-12">
+                        @foreach($m as $key => $value)
+                        <div class="card vg-blue">
+                            <div class="card-body"  data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        使用英雄：
+                                    </div>
+                                    <div class="col-md-5">0</div>
+                                    <div class="col-md-2">
+                                        蓝色方 : <br />
+                                        @foreach($value[0]['player_data'] as $k => $v)
+                                        {{ $v->name }}<br/>
+                                        @endforeach
+                                    </div>
+                                    <div class="col-md-2">
+                                        红色方 : <br />
+                                        @foreach($value[1]['player_data'] as $k => $v)
+                                            {{ $v->name }}<br/>
+                                        @endforeach
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="collapse" id="collapseExample">
+                                <div class="card card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+                                </div>
+                            </div>
+                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <footer class="footer ">
+    <footer class="footer">
         <div class="container">
             <div class="copyright pull-right">
                 &copy;
